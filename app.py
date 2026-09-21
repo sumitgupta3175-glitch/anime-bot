87da07924b6a383568012037486b05ba843f2961
sumitgupta3175-glitch <sumitgupta3175@gmail.com>
2026-09-21 19:28:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -398,5 +398,6 @@ if __name__ == "__main__":
     bot_thread.daemon = True
     bot_thread.start()
     
-    demo.launch(server_name="0.0.0.0", server_port=7860)
-    
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
+                              
